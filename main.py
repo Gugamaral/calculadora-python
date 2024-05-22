a2c6f3c0512ae88a39446ed182f9b246c47399a5
Gustavo Amaral <gugamaral@gmail.com>
2024-05-21 21:24:09 -0300

    Versão 1.0

---
+++ main.py
@@ -0,0 +1,8 @@
+# usuário informa os números
+x = str(input('Informe o primeiro número')).replace(',', '.')
+y = str(input('Informe o segundo número')).replace(',', '.')
+
+# converte para números decimais
+x = float(x)
+y = float(y)
+
