79abcceba89d5e1bbbeb09824ac9465df3dc84c7
Gustavo Amaral <gugamaral@gmail.com>
2024-05-21 21:45:22 -0300

    Versão 1.2

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# usuário informa os números
-x = str(input('Informe o primeiro número')).replace(',', '.')
-y = str(input('Informe o segundo número')).replace(',', '.')
+while True:
 
-# converte para números decimais
-x = float(x)
-y = float(y)
+    # usuário informa os números
+    x = str(input('Informe o primeiro número ')).replace(',', '.')
+    y = str(input('Informe o segundo número ')).replace(',', '.')
 
-print('Informe a operação que deseja fazer: \n')
-print('"+" para somar.')
-print('"-" para subtrair.')
-print('"*" para multiplicar.')
-print('"/" para dividir.')
-print('"%" para encontrar o resto da divisão.')
+    # converte para números decimais
+    x = float(x)
+    y = float(y)
 
-op = input('Operação desejada: ')
+    print('Operações disponíveis: \n')
+    print('"+" para somar.')
+    print('"-" para subtrair.')
+    print('"*" para multiplicar.')
+    print('"/" para dividir.')
+    print('"%" para encontrar o resto da divisão.')
+
+
+    op = input('Operação desejada: ')
+
+match op:
+    case '+':
+        print(f'A soma é: {x + y}.')
+    case '-':
+        print(f'A subtração é: {x - y}.')
+    case '*':
+        print(f'A multiplicação é: {x * y}.')
+    case '/':
+        print(f'A divisão é: {x / y}.')
+    case '%':
+        print(f'O resto da disvisão é: {x % y}.')
